84157e309c7d324d9e9ead8c578dc76c61f3101c
vwxyutarooo <yutaro@studio-rubbish.com>
2018-08-06 22:10:11 +0900

    feat: tweak script

--- history.js
+++ history.js
@@ -1,15 +1,16 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
 const fs = require('fs');
 const path = require('path');
 
 
-const argv = yargs.option('text', { alias: 't', default: false }).argv;
+// const argv = yargs.option('text', { alias: 't', default: false }).argv;
 const file = path.resolve(__dirname, './log.txt');
+const { HOOK_NAME } = process.env;
 
-if (argv.text) {
+if (HOOK_NAME) {
   if (fs.existsSync(file)) {
-    console.log(`Append ${argv.text} to the log.txt file.`);
-    fs.appendFileSync(file, `${argv.text}\n`);
+    console.log(`Append ${HOOK_NAME} to the log.txt file.`);
+    fs.appendFileSync(file, `${HOOK_NAME}\n`);
   }
 } else {
   fs.writeFileSync(file, '');
