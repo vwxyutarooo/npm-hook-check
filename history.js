f31aecadeedceae2ba7f960c0fc33bb0eb1211e1
vwxyutarooo <yutaro@studio-rubbish.com>
2018-08-08 01:58:21 +0900

    feat: ignore log file

--- history.js
+++ history.js
@@ -8,10 +8,12 @@ const file = path.resolve(__dirname, './log.txt');
 const { HOOK_NAME } = process.env;
 
 if (HOOK_NAME) {
-  if (fs.existsSync(file)) {
-    console.log(`Append ${HOOK_NAME} to the log.txt file.`);
-    fs.appendFileSync(file, `${HOOK_NAME}\n`);
+  if (!fs.existsSync(file)) {
+    fs.writeFileSync(file, '');
   }
+
+  console.log(`Append ${HOOK_NAME} to the log.txt file.`);
+  fs.appendFileSync(file, `${HOOK_NAME}\n`);
 } else {
   fs.writeFileSync(file, '');
   console.log('Clear the log.txt file.');
