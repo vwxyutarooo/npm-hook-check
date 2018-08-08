3610cd0192214d8dc2aadcc8dab432bac491886c
vwxyutarooo <yutaro@studio-rubbish.com>
2018-08-08 11:46:49 +0900

    feat: apply gitignore update

--- history.js
+++ history.js
@@ -1,9 +1,7 @@
-// const yargs = require('yargs');
 const fs = require('fs');
 const path = require('path');
 
 
-// const argv = yargs.option('text', { alias: 't', default: false }).argv;
 const file = path.resolve(__dirname, './log.txt');
 const { HOOK_NAME } = process.env;
 
