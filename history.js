a8d981584a31be18392c121a7cdf7c45752df34c
vwxyutarooo <yutaro@studio-rubbish.com>
2018-08-09 19:21:02 +0900

    console.log

--- history.js
+++ history.js
@@ -1,20 +1,2 @@
-// const yargs = require('yargs');
-const fs = require('fs');
-const path = require('path');
-
-
-// const argv = yargs.option('text', { alias: 't', default: false }).argv;
-const file = path.resolve(__dirname, './log.txt');
 const { HOOK_NAME } = process.env;
-
-if (HOOK_NAME) {
-  if (!fs.existsSync(file)) {
-    fs.writeFileSync(file, '');
-  }
-
-  console.log(`Append ${HOOK_NAME} to the log.txt file.`);
-  fs.appendFileSync(file, `${HOOK_NAME}\n`);
-} else {
-  fs.writeFileSync(file, '');
-  console.log('Clear the log.txt file.');
-}
+console.log('hook ------->', HOOK_NAME);
